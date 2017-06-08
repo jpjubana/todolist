e4bde6cb34872ddfff5ecb6b35c1099e26db141d
JP Ubana <jpjubana@gmail.com>
2017-06-08 16:03:55 -0400

    im fucked

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-// Add your JS here.
+    var newTaskInput = document.getElementById("new-todo");
+    var todolistContainer = document.getElementById("todo-list");
+
+    function(event) {
+        if (event.which == 13 || event.keyCode == 13) {
+            return false;
+        }
+        return true;
+    };
+
+    newTaskInput.addEventListener('keypress', function(event) {
+        var taskName = newTaskInput.value;
+        newTaskInput.value = "";
+        todolistContainer.insertAdjacentHTML('afterbegin', taskName);
+    });
